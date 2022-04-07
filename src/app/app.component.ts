e899328bb98466ba50790f016fc0d45932eb2c96
karcodehub <karbackupger.2020@gmail.com>
2022-04-07 12:48:18 +0200

    basic_logic_update1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,31 @@ export class AppComponent {
   title = 'firstApp';
   openNext=false;
   openNext1:boolean=false;
-  
+  openNext2:boolean=false;
+  openNext3:boolean=false;
+
   arrivalHandler(){
     console.log("Hii");
     this.openNext=true;
   }
+  
   arrivalHandler1(){
-    console.log("Hii");
-    this.openNext1=true;
+    console.log("Hii1");
+   
+    this.openNext1=!this.openNext1;
+    this.openNext3=false;
+  }
+
+  arrivalHandler2(){
+    console.log("Hii2");
+    
+    this.openNext2=!this.openNext2;
+
+  }
+
+  arrivalHandler3(){
+    console.log("Hii3");
+    this.openNext3=!this.openNext3;
+    this.openNext1=this.openNext2=false;
   }
 }
