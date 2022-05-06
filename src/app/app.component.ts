ad70d6a94813fede45e697254678035ecc38456c
karcodehub <karbackupger.2020@gmail.com>
2022-05-06 15:59:02 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,11 @@ export class AppComponent {
    
     this.openDiagnois=!this.openDiagnois;
     this.openIOT=false;
+    this.openPhy=false;
+    this.openRear=false;
+    this.openE32=false;
+    this.openE56=false;
+    this.openLoc=false;
   }
 
   arrivalHandlerRepair(){
@@ -45,27 +50,82 @@ export class AppComponent {
   arrivalHandlerPhy(){
     console.log("Phy");
     this.openPhy=!this.openPhy;
+    this.openRear=false;
+    this.openE32=false;
+    this.openE56=false;
+    this.openLoc=false;
   }
 
   arrivalHandlerRear(){
     console.log("Rear");
     this.openRear=!this.openRear;
+    this.openPhy=false;
+    this.openE32=false;
+    this.openE56=false;
+    this.openLoc=false;
   }
   arrivalHandlerE56(){
     console.log("E56");
     this.openE56=!this.openE56;
+    this.openPhy=false;
+    this.openRear=false;
+    this.openE32=false;
+    this.openLoc=false;
   }
   arrivalHandlerE32(){
     console.log("E32");
     this.openE32=!this.openE32;
+    this.openPhy=false;
+    this.openRear=false;
+    this.openE56=false;
+    this.openLoc=false;
   }
   arrivalHandlerLoc(){
     console.log("Loc");
     this.openLoc=!this.openLoc;
+    this.openPhy=false;
+    this.openRear=false;
+    this.openE32=false;
+    this.openE56=false;
   }
 
   arrivalHandlerDeploy(){
     console.log("Dep");
+    window.location.href="https://www.youtube.com/";
+  }
+  
+  arrivalHandlerStorage(){
+    console.log("Str");
+    window.location.href="https://www.youtube.com/";
+  }
+
+  arrivalHandlerShip(){
+    console.log("Ship");
+    window.location.href="https://www.youtube.com/";
+  }
+
+  arrivalHandlerPCB(){
+    console.log("Dep");
+    window.location.href="https://www.youtube.com/";
+  }
+  
+  arrivalHandlerSOPRear(){
+    console.log("Str");
+    window.location.href="https://www.youtube.com/";
+  }
+
+  arrivalHandlerSOPE32(){
+    console.log("Ship");
+    window.location.href="https://www.youtube.com/";
+  }
+
+  arrivalHandlerSOPE56(){
+    console.log("Str");
+    window.location.href="https://www.youtube.com/";
+  }
+
+  arrivalHandlerSOPLoc(){
+    console.log("Ship");
     window.location.href="https://www.youtube.com/";
   }
 }
