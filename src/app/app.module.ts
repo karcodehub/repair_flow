b70ee19bbb1e9ce855478769047a82e5b2ffc37e
karcodehub <karbackupger.2020@gmail.com>
2022-05-09 10:34:53 +0200

    2nd with toold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolsComponent } from './tools/tools.component';
+import { FlowchartComponent } from './flowchart/flowchart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolsComponent,
+    FlowchartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tools/tools.component.ts
@@ -0,0 +1,58 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
+export interface AvailableData {}
+
+@Component({
+  selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.css']
+})
+export class AvailableStocksComponent implements OnInit {
+  @Input() activeDrugList: any;
+  activeDrugListCopy: any;
+  p = 1;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort();
+
+  displayedColumns: string[] = [
+    'No.',
+    'DC Code',
+    'Product Name',
+    'Batch Number',
+    'Manuf Date',
+    'Expiry Date',
+    'Quantity',
+  ];
+  dataSource1: any;
+  dataSource: any;
+  pageSize: '7';
+  filterCheck: any;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor() {}
+  ngOnInit(): void {
+    this.activeDrugListCopy = this.activeDrugList;
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  public doFilter = (value: any) => {
+    console.log(this.filterCheck, value);
+    //this.activeDrugList=["Sagar", "sagar", "Bhoom", "asha", "mahesh", "Mahesh"];
+    this.activeDrugList = this.activeDrugListCopy;
+    let activeDrugList2 = this.activeDrugList.filter(
+      (item: any) => item.productName.toLowerCase().match(value.toLowerCase())
+      // console.log(item.productName, value);
+    );
+    this.activeDrugList = activeDrugList2;
+    console.log(activeDrugList2);
+  };
+}
