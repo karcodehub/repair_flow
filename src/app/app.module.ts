a1c72627a9372def004b82f90bbc50a35fa3ff9a
karcodehub <karbackupger.2020@gmail.com>
2022-05-16 11:55:36 +0200

    serach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridModule, PagerModule, PageService, SortService, FilterService } from '@syncfusion/ej2-angular-grids';
 import { ToolsComponent } from './tools/tools.component';
 import { FlowchartComponent } from './flowchart/flowchart.component';
 
 @NgModule({
   declarations: [AppComponent, ToolsComponent, FlowchartComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, GridModule, PagerModule],
+  providers: [PageService, SortService, FilterService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
+import { FilterSettingsModel } from '@syncfusion/ej2-grids';
 
 @Component({
   selector: 'app-tools',
-  templateUrl: './tools.component.html',
+  template: `<ejs-grid [dataSource]="data" [allowSorting]="true" [allowFiltering]="true"
+  [allowPaging]="true" [filterSettings]='filterOptions' [pageSettings]='pageSettings'>
+    <e-columns>
+      <e-column field='ProdID' headerText='Order ID' textAlign='left'  ></e-column>
+      <e-column field='ProductName' headerText='Product Name'></e-column>
+      <e-column field='Price' headerText='Price'></e-column>
+      <e-column field='ProdID' headerText='Order ID'></e-column>
+    </e-columns>
+  </ejs-grid>`,
+ // templateUrl: './tools.component.html',
   styleUrls: ['./tools.component.css']
 })
 export class ToolsComponent implements OnInit {
@@ -12,4 +23,56 @@ export class ToolsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  title= 'my-grid-app';
+  public filterOptions : FilterSettingsModel ={
+    ignoreAccent: true,
+    type: "Excel"
+  }
+  public pageSettings: PageSettingsModel = { pageSize: 5};
+  public data: object[] = [
+    {
+      ProdID: 1,
+      ProductName: "AAA",
+      Price: "123",
+      Country: "CHN",
+      Manufacturer: "Wurth",
+      Discount: "5%"
+    },
+
+    {
+      ProdID: 2,
+      ProductName: "BBB",
+      Price: "234",
+      Country: "CHN",
+      Manufacturer: "Wurth",
+      Discount: "6%"
+    },
+
+    {
+      ProdID: 3,
+      ProductName: "CCC",
+      Price: "345",
+      Country: "CHN",
+      Manufacturer: "Wurth",
+      Discount: "7%"
+    },
+
+    {
+      ProdID: 4,
+      ProductName: "DDD",
+      Price: "456",
+      Country: "CHN",
+      Manufacturer: "Wurth",
+      Discount: "8%"
+    },
+
+    {
+      ProdID: 5,
+      ProductName: "EEE",
+      Price: "567",
+      Country: "CHN",
+      Manufacturer: "Wurth",
+      Discount: "9%"
+    }
+  ]
 }
