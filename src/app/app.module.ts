1ca4408f3b0a5f532dceb8e93cdaf6fee9872d0b
karcodehub <karbackupger.2020@gmail.com>
2022-05-18 11:09:56 +0200

    shifted_imports_to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
+import { FilterSettingsModel } from '@syncfusion/ej2-grids';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +15,7 @@ import { FlowchartComponent } from './flowchart/flowchart.component';
 
 @NgModule({
   declarations: [AppComponent, ToolsComponent, FlowchartComponent],
-  imports: [BrowserModule, AppRoutingModule, GridModule, PagerModule, ],
+  imports: [BrowserModule, AppRoutingModule, GridModule, PagerModule, MatPaginatorModule,MatSort,MatSortModule ],
   providers: [PageService, SortService, FilterService],
   bootstrap: [AppComponent],
 })
